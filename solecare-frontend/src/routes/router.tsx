1825bde2ac7da42684d70942a7a3a259908eff40
Advaith H L <niceadvaith@gmail.com>
2025-06-24 10:37:42 +0200

    Move pages to auth directory

--- solecare-frontend/src/routes/router.tsx
+++ solecare-frontend/src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import { HomeLayout } from "../components/layouts/home";
 import { HomePage } from "../components/pages/home";
-import LoginPage from "../components/pages/login";
-import RegisterPage from "../components/pages/register";
+import LoginPage from "../components/pages/auth/login";
+import RegisterPage from "../components/pages/auth/register";
 import PatientDashboard from "../components/pages/patient/dashboard";
 import AssignDoctorPage from "../components/pages/patient/doctor";
 import DoctorDashboard from "../components/pages/doctor/dashboard";
